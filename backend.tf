0705e52dd25343360095087f0eb1e6180b61d8fa
mfaizalbe <mdfaizalehsan@gmail.com>
2026-05-08 20:37:17 +0800

    add versions.tf

--- backend.tf
+++ backend.tf
@@ -4,18 +4,4 @@ terraform {
     key    = "shortener-demo.tfstate"
     region = "ap-southeast-1"
   }
-  
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-
-    http = {
-      source  = "hashicorp/http"
-      version = "~> 3.0"
-    }
-  }
 }
