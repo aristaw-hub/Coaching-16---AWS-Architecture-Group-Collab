03ad50572c5de7f7b872107120b12f575ac08db5
Arista <aristaw@gmail.com>
2026-05-08 18:08:44 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,20 @@
 variable "group_name" {
-  default = "group5"
+  description = "Project group name used for resource naming"
+  type        = string
+  default     = "group5"
 }
 
 variable "domain_name" {
-  default = "group5-urlshortener.sctp-sandbox.com"
+  description = "Custom domain assigned for the URL shortener"
+  type        = string
+  default     = "group5-urlshortener.sctp-sandbox.com"
 }
 
 variable "my_ip" {
-  description = "Your public IP"
-  default     = "0.0.0.0/32" # UPDATE THIS
+  #description = "Your public IP"
+  #type         = string
+  #default     = "0.0.0.0/32" # UPDATE THIS
+  description  = "The IP range allowed to access the API. Set to 0.0.0.0/0 to allow global access."
+  type         = string
+  default     = "0.0.0.0/0"
 }
